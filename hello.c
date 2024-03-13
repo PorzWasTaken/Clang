060296dcc8e7ada5822a0e544564dda6093dac38
Porz <104210311+PorzWasTaken@users.noreply.github.com>
2024-03-13 22:04:45 +0700

    Add files via upload

---
+++ hello.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include "cs50.h" #gcc .\hello.c cs50.h(To update code runner) #.\a.exe
+                  
+int main(void)
+{
+    string name = get_string("What's your name bud? ");
+    printf("Hello, %s\n", name);
+    return 0;
+}
